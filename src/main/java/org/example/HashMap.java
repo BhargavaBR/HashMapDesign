8252094673b48747de873b2b3baef3ea17f902e1
bhargav <bhargavva.r@lenskart.in>
2025-03-14 17:57:09 +0530

    Implemented HashMap Datastructure internal working

---
+++ src/main/java/org/example/HashMap.java
@@ -0,0 +1,58 @@
+package org.example;
+
+public class HashMap {
+    public static final int SIZE = 31;
+    public Entry[] entries = new Entry[SIZE];
+
+    public class Entry{
+        private String key;
+        private String value;
+        private Entry next;
+
+        public Entry(String key, String value){
+            this.key = key;
+            this.value = value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+    public Entry getValue(String key){
+        int hash = Math.abs(key.hashCode()) % SIZE;
+        Entry e = entries[hash];
+        if(e!=null){
+            while (e.key.equals(key)) {
+                return e;
+            }
+        }
+        return null;
+    }
+    public void put(String key, String value){
+        int hash = Math.abs(key.hashCode()) % SIZE;
+        System.out.println(key.hashCode() +":"+hash);
+        Entry e = entries[hash];
+        if(e!=null){
+
+                while(e.next != null){
+                    if(e.key.equals(key)){
+                        e.value = value;
+                        return;
+                    }
+                    e = e.next;
+                }
+                e.next = new Entry(key, value);
+
+        }else{
+            entries[hash] = new Entry(key, value);
+        }
+    }
+}
